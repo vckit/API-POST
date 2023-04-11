3a5248e4679ae87edd20cc53ec4ed780893531b2
Abudlkhakim Magomedov <thevckit@gmail.com>
2023-04-11 22:46:02 +0300

    Добавил проверку пароля на корректность

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private EditText emailEditText;
+    private EditText passwordEditText;
     private Button sendButton;
+    private Button checkPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
         sendButton = findViewById(R.id.sendButton);
+        checkPasswordButton = findViewById(R.id.checkPasswordButton);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,9 +40,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        checkPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = passwordEditText.getText().toString();
+                if (isSecurePassword(password)) {
+                    Toast.makeText(MainActivity.this, "Password is secure", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Password is not secure", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    private boolean isValidEmail(CharSequence target) {
+    private boolean isSecurePassword(String password) {
+        int minLength = 8;
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialChar = false;
+        String specialCharacters = "!@#$%&*";
+
+        if (password.length() < minLength) {
+            return false;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            char currentChar = password.charAt(i);
+            if (Character.isUpperCase(currentChar)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(currentChar)) {
+                hasLowerCase = true;
+            } else if (Character.isDigit(currentChar)) {
+                hasDigit = true;
+            } else if (specialCharacters.contains(String.valueOf(currentChar))) {
+                hasSpecialChar = true;
+            }
+        }
+
+        return hasUpperCase && hasLowerCase && hasDigit && hasSpecialChar;
+    }
+
+
+        private boolean isValidEmail(CharSequence target) {
         if (target == null) {
             return false;
         }
